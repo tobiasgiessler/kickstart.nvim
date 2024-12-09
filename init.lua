ea3ab68f5557e1e6ed3f120a728c2cf9c6c6a411
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2024-12-09 22:09:33 +0100

    Navigate visual lines up and down

--- init.lua
+++ init.lua
@@ -193,6 +193,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Keybinds to make navigation of long lines easier.
+vim.keymap.set({ 'n', 'x' }, 'j', 'gj', { desc = 'Move down graphical lines' })
+vim.keymap.set({ 'n', 'x' }, 'k', 'gk', { desc = 'Move up graphical lines' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
