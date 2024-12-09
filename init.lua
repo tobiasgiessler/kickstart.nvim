4f94dfaf247f06fb3ccfbe950043c16bd30c51a1
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2024-12-09 22:09:16 +0100

    When wrapping lines do not split words

--- init.lua
+++ init.lua
@@ -157,6 +157,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Wrap lines by wrap whole words only.
+vim.opt.linebreak = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
